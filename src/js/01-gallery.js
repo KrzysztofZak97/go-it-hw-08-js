02a0acf409528ae811293937c79faf3eeecad6b9
KrzysztofZak97 <krzysztofzak60@gmail.com>
2023-04-14 11:52:50 +0200

    update gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+for (const i of galleryItems) {
+  const liEl = `<li><a class="gallery__item" href=${i.original}>
+  <img class="gallery__image" src="${i.preview}" alt=${i.description} />
+</a></li>`;
+  galleryEl.insertAdjacentHTML('beforeend', liEl);
+}
+
+galleryEl.addEventListener('click', e => {
+  e.preventDefault();
+});
+var lightbox = new SimpleLightbox('.gallery li a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getCurrentTime().then(function (second) {
+  console.log('title:', title);
+});
