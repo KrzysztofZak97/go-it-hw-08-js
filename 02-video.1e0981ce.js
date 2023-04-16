3d9c3372012c80e274ea46d8fa3e38fdb3b71a35
KrzysztofZak97 <116719670+KrzysztofZak97@users.noreply.github.com>
2023-04-16 18:04:09 +0000

    Deploying to gh-pages from @ KrzysztofZak97/go-it-hw-08-js@b9de7bd51917553dca21c7013aa965a03e4ab656 🚀

--- 02-video.1e0981ce.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getCurrentTime().then((function(e){console.log("title:",title)}));
-//# sourceMappingURL=02-video.1e0981ce.js.map
--- 02-video.42a6405f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getCurrentTime().then((function(e){console.log("title:",title)}))}();
-//# sourceMappingURL=02-video.42a6405f.js.map
