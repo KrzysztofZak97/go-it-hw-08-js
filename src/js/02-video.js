56c0bad2cf7e781f8d068862aca3f629a3cd6b14
KrzysztofZak97 <krzysztofzak60@gmail.com>
2023-04-16 21:28:59 +0200

    task 2 update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,12 @@
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const saveTime = throttle(seconds => {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem('videoplayer-current-time', seconds.seconds);
 }, 1000);
 
-player.on('play', saveTime);
+player.on('timeupdate', saveTime);
 
-player.setCurrentTime().then(function (seconds) {
-  localStorage.getItem('videoplayer-current-time');
-});
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
