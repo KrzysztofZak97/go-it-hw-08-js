2eddb63cd4b2ea66894013ff31238290c2d4a4e2
KrzysztofZak97 <krzysztofzak60@gmail.com>
2023-04-17 20:00:18 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input[name="email"]');
+const textEl = document.querySelector('textarea[name="message"]');
+
+const saveText = () => {
+  const formText = {
+    email: emailEl.value,
+    message: textEl.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formText));
+};
+
+const getText = () => {
+  const formLoad = JSON.parse(localStorage.getItem('feedback-form-state'));
+  emailEl.value = formLoad.email;
+  textEl.value = formLoad.message;
+};
+
+const delteText = () => {
+  const formDelte = JSON.parse(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+  emailEl.value = '';
+  textEl.value = '';
+  console.log('Form data cleared:', formDelte);
+};
+
+emailEl.addEventListener('input', throttle(saveText, 500));
+textEl.addEventListener('input', throttle(saveText, 500));
+
+document.addEventListener('DOMContentLoaded', getText);
+
+formEl.addEventListener('submit', ev => {
+  ev.preventDefault();
+  delteText();
+});
